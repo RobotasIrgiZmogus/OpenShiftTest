c9c04656e553fd54d3116b4dccb78ab25c648559
R0807 <r0807@example.com>
2018-01-29 18:56:09 +0200

    Add manifest to code

--- src/main/java/lt/robot/API.java
+++ src/main/java/lt/robot/API.java
@@ -2,11 +2,16 @@ package lt.robot;
 
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Component
 @Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class API {
 
     @Path("helloworld")
